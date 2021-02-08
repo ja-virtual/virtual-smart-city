c3f5fed4b2f7c9201314e1f02df8cfc73538970a
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-02-08 20:25:24 +0100

    scripts .sh update

--- virtual-smart-city/src/main/scripts/launch.app.vsc.sh
+++ virtual-smart-city/src/main/scripts/launch.app.vsc.sh
@@ -1,1 +1,4 @@
-mvn clean install
+m2=${M2_REPOS}
+clsspth=src/main/resources
+clsspth+=${clsspth}:${m2}/upec/ing1/pds/vsc/virtual-smart-city/0.0.1-SNAPSHOT/virtual-smart-city-0.0.1-SNAPSHOT.jar
+exec java -cp ${clsspth} upec.ing1.pds.vsc.VSCBackServer.SmartCityAppService $*
