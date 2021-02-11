f7e61b51afe91eb00ba093833acbeb7325472ce7
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-02-11 19:23:26 +0100

    Script bat update

--- virtual-smart-city/src/main/scripts/launch.app.vsc.bat
+++ virtual-smart-city/src/main/scripts/launch.app.vsc.bat
@@ -1,3 +1,4 @@
-set m2=%M2_REPO%
-set clsspth=%m2%\upec\ing1\pds\vsc\virtual-smart-city\0.0.1-SNAPSHOT\virtual-smart-city-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-call java -cp %clsspth% upcdec.ing1.pds.vsc.VSCBackServer.SmartCityAppService %*
+set m2=%M2_REPO
+set clsspth=%m2%\edu\si\ing1\pds\vsc\virtual-smart-city\1.0-SNAPSHOT\virtual-smart-city-1.0-SNAPSHOT-jar-with-dependencies.jar
+call java -cp %clsspth% edu.si.ing1.pds.vsc.server.SmartCityAppService %*
+
