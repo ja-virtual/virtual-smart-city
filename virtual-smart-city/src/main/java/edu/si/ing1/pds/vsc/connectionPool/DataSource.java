7a3c912dfcce878bb3b47d60616a7dbb03ce216e
EL HOURRI Hajar <elorihajar@gmail.com>
2021-03-14 16:00:57 +0100

    Code update

--- virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/connectionPool/DataSource.java
+++ virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/connectionPool/DataSource.java
@@ -2,14 +2,61 @@ package edu.si.ing1.pds.vsc.connectionPool;
 
 import java.sql.Connection;
 
-public class DataSource {
+public class DataSource extends Thread {
 
+    //attributs
 	static JDBCConnectionPool conPool=new JDBCConnectionPool ();
-	public static Connection takeCon()
+    private int connection_interval;
+
+	public int getMaxConnection()
+	{
+		return conPool.getMax_connection();
+	}
+	public int getAvailableConnection()
+	{
+		return conPool.getAvailable_connection();
+	}
+	public int getUsedConnection()
+	{
+		return conPool.getUsed_connection();
+	}
+	public void setMaxConnection(int max_con)
+	{
+		conPool.setMax_connection(max_con);
+	}
+	public void setAvailableConnection(int available_con)
+	{
+		conPool.setAvailable_connection(available_con);
+	}
+	public void settUsedConnection(int used_con)
+	{
+		 conPool.setUsed_connection(used_con);
+	}
+	
+	//constructor
+	public DataSource(int max_con, int av_con, int con_interv) {
+		connection_interval=con_interv;
+	}
+	
+	// run method
+   public void run()
+   {
+	ConnectionDB condb=new ConnectionDB();
+	conPool.returnCon(condb);
+	try {
+		sleep(connection_interval);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+   }
+	
+	//methods
+	public static ConnectionDB takeCon()
 	{
 		return conPool.connectionEntity();
 	}
-	public static void returnCon(Connection con)
+	public static void returnCon(ConnectionDB con)
 	{
 		conPool.returnCon(con);
 	}
@@ -17,4 +64,6 @@ public class DataSource {
 	{
 		conPool.Close();
 	}
+	
+	
 }
--- virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/server/SmartCityAppService.java
+++ virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/server/SmartCityAppService.java
@@ -5,29 +5,40 @@ import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.si.ing1.pds.vsc.connectionPool.DataSource;
+
 		public class SmartCityAppService {
+			public static DataSource ds=null;
 			private final static Logger logger = LoggerFactory.getLogger(SmartCityAppService.class.getName());
 			public static void main(String[] args) throws Exception {
 
 				Options options = new Options();
-				Option connectionLimit  = new Option("connectionLimit", "connectionLimit", true, "connectionLimit variable");
-				Option workingTrial  = new Option("workingTrial", "workingTrial", true, "workingTrial variable");
-				options.addOption(connectionLimit);
-				options.addOption(workingTrial);
+				Option max_connection  = new Option("max_con", "maxConnection", true, "the number of connections that we could possibly create to be used");
+				Option available_connection  = new Option("av_con", "availableConnection", true, "the number of connection that are available to be used at the moment");
+				Option connection_duration  = new Option("con_dur", "connectionDuration", true, "the duration accorded to the user to connect to the database");
+				options.addOption(max_connection);
+				options.addOption(available_connection);
+				options.addOption(connection_duration);
 				CommandLineParser parser = new DefaultParser();
 				HelpFormatter formatter = new HelpFormatter();
 				CommandLine commandLine;
 				commandLine = parser.parse(options, args);
-				int connectionLimit_i = 40;
-				if (commandLine.hasOption("connectionLimit"))
-					connectionLimit_i = Integer.parseInt(commandLine.getOptionValue("connectionLimit"));
+				int max_connection_i =0;
+				if (commandLine.hasOption("maxConnection"))
+					max_connection_i = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
 
-				boolean workingTrial_i = false;
-				if (commandLine.hasOption("workingTrial"))
-					workingTrial_i = Boolean.parseBoolean(commandLine.getOptionValue("workingTrial"));
-
-				logger.info("VSC Application is running, the workingTrial = "+ workingTrial_i +" & connectionLimit = " +connectionLimit_i+".");
-
+			    int available_connection_i = 0;
+				if (commandLine.hasOption("availableConnection"))
+					available_connection_i = Integer.parseInt(commandLine.getOptionValue("availableConnection"));
+ 
+				int connection_duration_i =0;
+				if (commandLine.hasOption("connectionDuration"))
+					connection_duration_i = Integer.parseInt(commandLine.getOptionValue("connectionDuration"));
+				logger.info("VSC Application is running, maximal_connection= "+max_connection_i +" & available_connection = " +available_connection_i+" & connection_duration = " +connection_duration_i+".");
+				ds = new DataSource(max_connection_i, available_connection_i, connection_duration_i);
+				ds.start();
+				//ds.setMaxConnection(max_connection_i);
+			//	ds.setAvailableConnection(available_connection_i);
 			}
 		}
 
