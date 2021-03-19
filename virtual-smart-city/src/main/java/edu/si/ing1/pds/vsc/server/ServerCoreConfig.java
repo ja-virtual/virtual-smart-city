66cf0c0555997486233e633d3c48a5296465912a
Mohand_SADAT <Tmouh3795@users.noreply.github.com>
2021-03-19 14:30:09 +0100

    VSC Server configuration for TCP/IP connection

---
+++ virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/server/ServerCoreConfig.java
@@ -0,0 +1,37 @@
+package edu.si.ing1.pds.vsc.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerCoreConfig {
+	
+     private int listenPort;
+     private int soTimeout;
+     
+     public ServerCoreConfig() {
+    	 
+     }
+
+	public int getListenPort() {
+		return listenPort;
+	}
+
+	public void setListenPort(int listenPort) {
+		this.listenPort = listenPort;
+	}
+
+	public int getSoTimeout() {
+		return soTimeout;
+	}
+
+	public void setSoTimeout(int soTimeout) {
+		this.soTimeout = soTimeout;
+	}
+
+	@Override
+	public String toString() {
+		return "ServerCoreConfig [listenPort=" + listenPort + ", soTimeout=" + soTimeout + "]";
+	}
+     
+     
+}
