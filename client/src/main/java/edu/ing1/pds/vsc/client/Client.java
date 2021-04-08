7f5754e494ea53e7640abd3a85c76778b7c7cdac
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-04-08 17:20:56 +0200

    Client update

--- client/src/main/java/edu/ing1/pds/vsc/client/Client.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/Client.java
@@ -23,18 +23,19 @@ public class Client extends Thread
         String[] operation=new String[] {"add","update","delete","read"};
        ClientConfig config=new ClientConfig();
        Socket client_test=new Socket(config.getConfig().getIp(),config.getConfig().getListenPort());
-		int i=1;
-		while(i<5)
-		{
-		PrintWriter out = new PrintWriter(client_test.getOutputStream(),true);
-		String operation_name=operation[new Random().nextInt(4)];
-		out.println(operation_name);
-		System.out.println("client  number "+i+"wants an /a "+operation_name+ "'s operation\n");
-		BufferedReader in = new BufferedReader(new InputStreamReader(client_test.getInputStream()));
-		String msg=in.readLine();
-		System.out.println("Server's response\n"+msg+"\n");
-		i++;
-		}
+			String msg="";
+			while(msg!=null)
+			{
+				msg=null;
+				PrintWriter out = new PrintWriter(client_test.getOutputStream(),true);
+				String operation_name=operation[new Random().nextInt(4)];
+				out.println(operation_name);
+				System.out.println("client  number "+i+"wants an /a "+operation_name+ "'s operation\n");
+				BufferedReader in = new BufferedReader(new InputStreamReader(client_test.getInputStream()));
+				msg=in.readLine();
+				if(msg!=null)
+					System.out.println("Server's response\n"+msg+"\n");
+			}
 		}catch (Exception ex)
 	{
 		ex.printStackTrace();
