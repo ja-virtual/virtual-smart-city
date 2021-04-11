65bf07d8cf3a509b1652153d1575235b157b0d43
ines.dorai <ines.dorai@esprit.tn>
2021-04-11 12:15:21 +0200

    Menu Interface updated

--- client/src/main/java/edu/ing1/pds/vsc/client/Menu.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/Menu.java
@@ -6,85 +6,143 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException; 
-class Menu extends JFrame implements ItemListener {
 
+public class Menu {
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	 // frame 
+    static JFrame frame; 
+    // combobox 
+    static JComboBox combobox; 
+    // label 
+    static JLabel l1; 
 
-	    // frame 
-	    static JFrame frame; 
-	    // combobox 
-	    static JComboBox combobox; 
-	    // label 
-	    static JLabel l1; 
+    public static void main(String[] args) throws IOException 
+    {
+        // créer un nouveau frame 
+        frame = new JFrame("frame"); 
+     //   ImageIcon icone = new ImageIcon("logo-ja-virtual.png");
+        BufferedImage img = ImageIO.read(new File("logo-ja-virtual.png"));
+        JLabel pic = new JLabel(new ImageIcon(img));
+        frame.add(pic);
+        // créer un objet
+        Menu obj = new Menu(); 
+  
+        // définir la disposition du frame 
+        frame.setLayout(new FlowLayout()); 
+  
+        // tableau de chaînes contenant des langages
+        String s1[] = {"EDF", "Apple", "Microsoft", "Windows", "Samsung" }; 
+      
+        // créer une case à cocher
+        combobox = new JComboBox(s1); 
 
-	    public static void main(String[] args) throws IOException 
-	    {
-	        // créer un nouveau frame 
-	        frame = new JFrame("frame"); 
-	     //   ImageIcon icone = new ImageIcon("logo-ja-virtual.png");
-	        BufferedImage img = ImageIO.read(new File("logo-ja-virtual.png"));
-	        JLabel pic = new JLabel(new ImageIcon(img));
-	        frame.add(pic);
-	        // créer un objet
-	        Menu obj = new Menu(); 
-	  
-	        // définir la disposition du frame 
-	        frame.setLayout(new FlowLayout()); 
-	  
-	        // tableau de chaînes contenant des langages
-	        String s1[] = {null, "EDF", "Apple", "Microsoft", "Windows", "Samsung" }; 
-	      
-	      //  combobox.setSelectedItem(null); // aucune sélection par défaut
-	        // créer une case à cocher
-	        combobox = new JComboBox(s1); 
-	  
-	        // ajouter ItemListener
-	        combobox.addItemListener(obj); 
-	  
-	        // créer des étiquettes
-	        l1 = new JLabel("Bienvenue sur Ja-Virtual\n"); 
-	    
-	  
-	        
-	        JButton btn = new JButton("Annuler");
-	        btn.setBounds(80,190,100,40);
-	        JButton btn2 = new JButton("Valider");
-	        btn2.setBounds(220,190,100,40);
-	        JButton btn3 = new JButton("Mairie");
-	        btn3.setBounds(150,250,100,40);
-	        btn3.setBackground(Color.green); 
-	        // créer un nouveau panneau
-	        JPanel p = new JPanel(); 
+	     combobox.setSelectedIndex(0); // aucune sélection par défaut
+        // ajouter ItemListener
+        combobox.addItemListener(obj); 
+  
+       combobox.setToolTipText("saisir le nom de votre entreprise");
+        
+        
+        
+        // créer des étiquettes
+        l1 = new JLabel("Bienvenue sur Ja-Virtual\n"); 
+    
+  
+        
+        JButton btn = new JButton("Annuler");
+        btn.setBounds(80,190,100,40);
+        JButton btn2 = new JButton("Valider");
+        btn2.setBounds(220,190,100,40);
+        JButton btn3 = new JButton("Mairie");
+        btn3.setBounds(150,250,100,40);
+        btn3.setBackground(Color.green); 
+        // créer un nouveau panneau
+        JPanel p = new JPanel(); 
 
-	        // ajouter combobox et labels au panneau
-	        p.add(l1);
-	        p.add(combobox); 
-	       
+        // ajouter combobox et labels au panneau
+        p.add(l1);
+        p.add(combobox); 
+       
 
-	        // ajouter le panneau au frame
-	        frame.add(p); 
+        // ajouter le panneau au frame
+        frame.add(p); 
 
-	        // définir la taille du frame 
-	        frame.setSize(400, 200); 
+        // définir la taille du frame 
+        frame.setSize(400, 200); 
 
-	        frame.show(); 
-	        
-	        
-	        frame.add(btn);
-	        frame.add(btn2);
-	        frame.add(btn3);
-	        frame.setSize(300,300);
-	        frame.setLayout(null);
+        frame.show(); 
+        
+        
+        frame.add(btn);
+        frame.add(btn2);
+        frame.add(btn3);
+        frame.setSize(300,300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    } 
+    public void itemStateChanged(ItemEvent e) 
+    { 
+        // si l'état du combobox est modifiée 
+        if (e.getSource() == combobox) { 
+  
+         //   l2.setText(" ["+combobox.getSelectedItem()+"]"); 
+        } 
+    } 
+ 
+}
+	
+	
+	
+	
+	
+
+	  private static JFrame frame = new JFrame();
+	  private static final String NOT_SELECTABLE_OPTION = " - Select an Option - ";
+	  private static final String NORMAL_OPTION = "Normal Option";
+
+	  public static void main(String[] args) throws Exception {
+	    JComboBox<String> comboBox = new JComboBox<String>();
+
+	    comboBox.setModel(new DefaultComboBoxModel<String>() {
+	      private static final long serialVersionUID = 1L;
+	      boolean selectionAllowed = true;
+
+	      @Override
+	      public void setSelectedItem(Object anObject) {
+	        if (!NOT_SELECTABLE_OPTION.equals(anObject)) {
+	          super.setSelectedItem(anObject);
+	        } else if (selectionAllowed) {
+	          // Allow this just once
+	          selectionAllowed = false;
+	          super.setSelectedItem(anObject);
+	        }
+	      }
+	    });
+
+	    comboBox.addItem(NOT_SELECTABLE_OPTION);
+	    comboBox.addItem(NORMAL_OPTION);
+
+	    frame.add(comboBox);
+	    frame.pack();
+	    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+	    SwingUtilities.invokeLater(new Runnable() {
+	      @Override
+	      public void run() {
 	        frame.setVisible(true);
-	    } 
-	    public void itemStateChanged(ItemEvent e) 
-	    { 
-	        // si l'état du combobox est modifiée 
-	        if (e.getSource() == combobox) { 
-	  
-	         //   l2.setText(" ["+combobox.getSelectedItem()+"]"); 
-	        } 
-	    } 
-	 
+	      }
+	    });
+	  }
+
+
+
 }
