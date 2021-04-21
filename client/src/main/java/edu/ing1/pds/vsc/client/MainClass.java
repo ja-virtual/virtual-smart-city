f306effda802a7bdb1a1c1c20eb0a05d6a99aa73
Coumba KANOUTE <kanoute.coumba@gmail.com>
2021-04-21 21:59:10 +0200

    Interface for electro-chromatic's window

---
+++ client/src/main/java/edu/ing1/pds/vsc/client/MainClass.java
@@ -0,0 +1,10 @@
+package edu.ing1.pds.vsc.client;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		 Welcome welcome = new Welcome("Virtual Smart City");
+	      		
+			}
+	
+}
