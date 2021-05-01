07d424e1c482fafc3455ffa78c62e4cd4260f90d
Mohand_SADAT <Tmouh3795@users.noreply.github.com>
2021-05-01 19:06:31 +0200

    Access Card Management Model https://github.com/ja-virtual/virtual-smart-city/projects/1#card-59784863

--- client/src/main/java/edu/ing1/pds/vsc/client/accessCardManagement/vscDataModel/GeneralServices.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/accessCardManagement/vscDataModel/GeneralServices.java
@@ -2,20 +2,20 @@ package edu.ing1.pds.vsc.client.accessCardManagement.vscDataModel;
 
 public class GeneralServices {
 
-	   private int id_generalServices;
+    private int id_generalServices;
 
-	public GeneralServices(int id_generalServices) {
-		super();
-		this.id_generalServices = id_generalServices;
-	}
+    public GeneralServices(int id_generalServices) {
+        super();
+        this.id_generalServices = id_generalServices;
+    }
 
-	public int getId_generalServices() {
-		return id_generalServices;
-	}
+    public int getId_generalServices() {
+        return id_generalServices;
+    }
 
-	public void setId_generalServices(int id_generalServices) {
-		this.id_generalServices = id_generalServices;
-	}
-	   
-	   
+    public void setId_generalServices(int id_generalServices) {
+        this.id_generalServices = id_generalServices;
+    }
+
+
 }
--- client/src/main/java/edu/ing1/pds/vsc/client/accessCardManagement/vscDataModel/TokenGeneratorModel.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/accessCardManagement/vscDataModel/TokenGeneratorModel.java
@@ -2,28 +2,32 @@ package edu.ing1.pds.vsc.client.accessCardManagement.vscDataModel;
 
 public class TokenGeneratorModel {
 
-	private int id_token;
-	
-	private enum token{
-		frojfoejzf4484ezaerth,
-		frojfoejzf4484ezae86d,
-		frojfoejzf4484ezaezef,
-		frojfoejzf4484ezae862,
-		frojfoejzf4484ezaeze5
-	};
-    
-	private int id_accesslevel;
-	public int getId_token() {
-		return id_token;
-	}
-	public void setId_token(int id_token) {
-		this.id_token = id_token;
-	}
-	public int getId_accesslevel() {
-		return id_accesslevel;
-	}
-	public void setId_accesslevel(int id_accesslevel) {
-		this.id_accesslevel = id_accesslevel;
-	}
+    private int id_token;
+
+    private enum token {
+        frojfoejzf4484ezaerth,
+        frojfoejzf4484ezae86d,
+        frojfoejzf4484ezaezef,
+        frojfoejzf4484ezae862,
+        frojfoejzf4484ezaeze5
+    };
+
+    private int id_accesslevel;
+
+    public int getId_token() {
+        return id_token;
+    }
+
+    public void setId_token(int id_token) {
+        this.id_token = id_token;
+    }
+
+    public int getId_accesslevel() {
+        return id_accesslevel;
+    }
+
+    public void setId_accesslevel(int id_accesslevel) {
+        this.id_accesslevel = id_accesslevel;
+    }
 
 }
