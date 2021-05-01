7d105fba640b0b0587d12944ae48909b5f88c82e
ines.dorai <ines.dorai@esprit.tn>
2021-05-01 21:27:37 +0200

    update analyzeIndicators

--- client/src/main/java/edu/ing1/pds/vsc/client/Interface1.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/Interface1.java
@@ -2,6 +2,7 @@ package edu.ing1.pds.vsc.client;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 
@@ -19,9 +20,20 @@ public class Interface1 extends JFrame{
 
 
 	JLabel image = new JLabel();
-	ImageIcon  img= new ImageIcon("logo-ja-virtual.png");
+	ImageIcon  img= new ImageIcon("logo_ja_virtual.png");
 	image.setIcon( new ImageIcon(img.getImage().getScaledInstance(75,75, Image.SCALE_SMOOTH)));
+	JLabel c1= new JLabel("Bienvenue %username% sur la pateforme de Virtual Smart City\n");
+	JLabel c2= new JLabel("La maitrise, l'expérience, vous êtes chez vous !");
+	c1.setFont(new Font("Serif", Font.BOLD, 25));
+	
+	
+	
+	setResizable(false);
+	setVisible(true);
 
 
-
-}}
+}
+public static void main (String[] args )
+{
+	new Interface1();}
+}
