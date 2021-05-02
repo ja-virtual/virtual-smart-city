d1ca9419738ef88a13ef7a96972d83f030b094d7
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-05-02 20:28:29 +0200

    Location test

--- client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferCreator.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferCreator.java
@@ -1,11 +1,23 @@
 package edu.ing1.pds.vsc.client.workspaceLocation.lolo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.ing1.pds.vsc.client.ClientToServer;
+import edu.ing1.pds.vsc.client.Request;
+
 public class OfferCreator {
 
     int openspace_number;
     int individual_office_number;
     int meetingroom_number;
     String type_floor;
-    Offer ofr;
+    ArrayList<Offer> final_offers_array;
+
+    ClientToServer connection = new ClientToServer();
+    
 
     public OfferCreator (int op_nbr, int io_nbr, int mr_nbr, String floor) {
         
@@ -13,45 +25,116 @@ public class OfferCreator {
         individual_office_number = io_nbr;
         meetingroom_number = mr_nbr;
         type_floor = floor;
-        ofr = new Offer();
-        
-        try
-        {
-            //send the openspace number requested
-            Request request=new Request();
+        final_offers_array = ArlOfr(OfferFromResult(ResultOp(),ResultMr(),ResultIo()));
+        		
+    }
+    
+    public ArrayList<Offer> ArlOfr (ArrayList<ArrayList<WorkSpace>> ofr) {
+    	int i = 0;
+    	ArrayList<Offer> offers = new ArrayList<Offer>();
+    	while(i < ofr.size()) {
+    		Offer arlfr = new Offer(ofr.get(i));
+    		offers.add(arlfr);
+    		i++;
+    	}
+		return offers;	
+    }
+  
+    public ArrayList<ArrayList<WorkSpace>> OfferFromResult(ArrayList<WorkSpace> arl_op,ArrayList<WorkSpace> arl_mr, ArrayList<WorkSpace> arl_io) {
+    	ArrayList<ArrayList<WorkSpace>> ArlOfr = new ArrayList<ArrayList<WorkSpace>>();   
+    	Collections.sort(arl_op, WorkSpace.IdBuildingComparator);
+    	Collections.sort(arl_io, WorkSpace.IdBuildingComparator);
+    	Collections.sort(arl_mr, WorkSpace.IdBuildingComparator);
+    	
+    	int a = 0;
+    	int b = 0;
+    	int c = 0;
+    	
+    	while(a < arl_op.size() || b < arl_io.size() || c < arl_mr.size()) {
+    		ArrayList<WorkSpace> oneArl = new ArrayList<WorkSpace>();
+    		int i = 0;
+    		while(i < openspace_number) {
+    			oneArl.add(arl_op.get(i));
+    			i++;
+    			a++;
+    		}
+    		int j = 0;
+    		while(j < individual_office_number) {
+    			oneArl.add(arl_io.get(j));
+    			j++;
+    			b++;
+    		}
+    		int k = 0;
+    		while(k < meetingroom_number) {
+    			oneArl.add(arl_mr.get(k));
+    			k++;
+    			c++;
+    		}
+    		ArlOfr.add(oneArl);
+    	}
+    	return ArlOfr;
+    }
+    public ArrayList<WorkSpace> ResultOp() {
+    	ArrayList<WorkSpace> arl_op = new ArrayList<WorkSpace>();
+    	
+    	try {
+	    	//send the openspace number requested
+	        Request request=new Request();
+	        request.setName_request("request_workspace");
+	        HashMap<String,Object>param=new HashMap<String,Object>();
+	        param.put("type_workspace","open Space");
+	        param.put("type_floor",type_floor);
+	        request.setData(param);
+	        Request response=connection.SendRequest(request);
+	        for(Map n :(ArrayList<Map>) response.getData())
+	        	arl_op.add(new WorkSpace((Integer)n.get("id_workspace"),(String)n.get("type_workspace"),(Integer)n.get("floor_number"),(Integer)n.get("id_building")));
+    	 }catch(Exception e){
+             
+         }
+ 		return arl_op;
+ 	}
+    public ArrayList<WorkSpace> ResultMr() {
+    	ArrayList<WorkSpace> arl_mr = new ArrayList<WorkSpace>();
+    	
+    	try {
+    		// send the meeting room number requested
+    		Request request=new Request();
             request.setName_request("request_workspace");
-            HashMap<String,Object>param=new HashMap<String,Object>();
-            param.put("requested_number",openspace_number);
-            param.put("type_workspace","open Space");
-            param.put("type_floor",type_floor);
-            request.setData(param);
-            Request response=connection.SendRequest(request);
-            ofr.openspaces=(ArrayList<Map>)response.getData();
-
-            // send the meeting room number requested
-            request=new Request();
-            request.setName_request("request_workspace");
-            param=new HashMap<String,Object>();
-            param.put("requested_number",meetingroom_number);
+            HashMap<String,Object> param=new HashMap<String,Object>();
             param.put("type_workspace","salle de reunion");
             param.put("type_floor",type_floor);
             request.setData(param);
-            response=connection.SendRequest(request);
-            ofr.meetingrooms=(ArrayList<Map>)response.getData();
+            Request response=connection.SendRequest(request);
+            for(Map n :(ArrayList<Map>) response.getData())
+            	arl_mr.add(new WorkSpace((Integer)n.get("id_workspace"),(String)n.get("type_workspace"),(Integer)n.get("floor_number"),(Integer)n.get("id_building")));
+    	 }catch(Exception e){
+             
+         }
+ 		return arl_mr;
+ 	}
 
+    public ArrayList<WorkSpace> ResultIo() {
+    	ArrayList<WorkSpace> arl_io = new ArrayList<WorkSpace>();
+    	try
+        {
             //send the individual office number requested
-            request=new Request();
+    		Request request=new Request();
             request.setName_request("request_workspace");
-            param=new HashMap<String,Object>();
-            param.put("requested_number",individual_number);
+            HashMap<String,Object> param=new HashMap<String,Object>();
             param.put("type_workspace","individuel");
             param.put("type_floor",type_floor);
             request.setData(param);
-            response=connection.SendRequest(request);
-            ofr.individual=(ArrayList<Map>)response.getData();
+            Request response=connection.SendRequest(request);
+            for(Map n :(ArrayList<Map>) response.getData())
+            	arl_io.add(new WorkSpace((Integer)n.get("id_workspace"),(String)n.get("type_workspace"),(Integer)n.get("floor_number"),(Integer)n.get("id_building")));
 
         }catch(Exception e){
             
         }
+		return arl_io;
     }
+
+    public static void main(String[] args) {
+    	OfferCreator Offers = new OfferCreator(2,2,1,"haut");
+    	 }
 }
--- client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferManager.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferManager.java
@@ -1,56 +1,73 @@
 package edu.ing1.pds.vsc.client.workspaceLocation.lolo;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class OfferManager {
 
-    offer first;
-    offer seconde;
-
-    public OfferManager(ArrayList Offers) {
+	public ArrayList<Offer> Offers;
+	
+	public OfferManager(ArrayList<Offer> Offers, int ask_price, int ask_area, int ask_employee_nbr){
+		Offers = OfferSorter(Offers, ask_price, ask_area, ask_employee_nbr);
+		this.Offers = Offers;
+	}
+	
+    public ArrayList<Offer> OfferSorter(ArrayList<Offer> Offers, int ask_price, int ask_area, int ask_employee_nbr) {
         for (int i = 0; i < Offers.size(); i++){
             Offer ofr_i = Offers.get(i);
-            int priice = ofr_i.theprice + (ofr_i.theprice*0.05);
-            if (priice > Location.price){
+            double priice = (ofr_i.theprice + (ofr_i.theprice*(0.05)));
+            if (priice > ask_price){
                 Offers.remove(i);
             }
             else {
-                int diff = (Location.price - ofr_i.theprice)/ofr_i.theprice;
-                doucble pricescore = diff*(-2);
+                double diff = (ask_price - (ofr_i.theprice))/(ofr_i.theprice);
+                double pricescore = diff*(-2);
                 ofr_i.thescore = ofr_i.thescore + pricescore;
             }
         }
-        for (int i; i < Offers.size(); i++){
+        for (int i = 0; i < Offers.size(); i++){
             Offer ofr_i = Offers.get(i);
-            int area = ofr_i.thearea + (ofr_i.thearea*0.1);
-            if (area < Location.area){
+            double area = ofr_i.thearea + (ofr_i.thearea*0.1);
+            if (area < ask_area){
                 Offers.remove(i);
             }
             else {
-                int diff = (area - Location.area)/Location.area;
+                double diff = (area - ask_area)/ask_area;
                 double areascore = diff*(-1);
                 ofr_i.thescore = ofr_i.thescore + areascore;
             } 
         }
-        for (int i; i < Offers.size(); i++){
+        for (int i=0; i < Offers.size(); i++){
             Offer ofr_i = Offers.get(i);
-            if (ofr_i.themax_employee_number > Location.employee_nbr){
+            if (ofr_i.themax_employee_number > ask_employee_nbr){
                 Offers.remove(i);
             }
             else {
-                int diff = (Location.employee_nbr - ofr_i.themax_employee_number)/ofr_i.themax_employee_number;
+                int diff = (ask_employee_nbr - ofr_i.themax_employee_number)/ofr_i.themax_employee_number;
                 double employeescore = diff*(-1);
                 ofr_i.thescore = ofr_i.thescore + employeescore;
             }
         }
-        HashMap<Offer, Integer> hm = new HashMap<Offer, Integer>();
+        Collections.sort(Offers, Offer.OfferComparator);
+       
+        
+    return Offers;
+    }
+}
+    
+    
+    
+    
+    
+    
+       /* HashMap<Offer, Integer> hm = new HashMap<Offer, Integer>();
         for (int i; i < Offers.size(); i++){
             Offer ofr_i = Offers.get(i);
             hm.put(ofr_i, ofr_i.thescore);
             //caster int en Integer ?
         }
 
-        //Plus qu'a trier la HashMap par Valeur et avoir les meillers offres !
+        //Plus qu'a trier la HashMap par Valeur et avoir les meillers offres !*/
 
 
 
@@ -59,17 +76,3 @@ public class OfferManager {
 
 
 
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-}
