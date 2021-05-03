9e97dad55d1be4707eec1f1d1a88853314b35feb
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-05-04 01:56:38 +0200

    update

--- virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/server/SmartCityAppServer.java
+++ virtual-smart-city/src/main/java/edu/si/ing1/pds/vsc/server/SmartCityAppServer.java
@@ -71,11 +71,12 @@ public class SmartCityAppServer extends Thread {
 				System.out.print("*********************\n ");
 				//this.serve();
 			} catch (Exception e1) {
-				 //e1.printStackTrace();
-				 //System.out.print("I am seing you\n ");
+				 //e1.printStackTrace()
 				this.serve();
+
 			}
 		}
+
 
 		try {
 			//in.close();
