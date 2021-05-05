50dfc6f5a396ad852f0b8fda6316cb31929044d0
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-05-05 15:08:19 +0200

    set Location UC update

--- client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferManager.java
+++ client/src/main/java/edu/ing1/pds/vsc/client/workspaceLocation/lolo/OfferManager.java
@@ -5,69 +5,69 @@ import java.util.HashMap;
 
 public class OfferManager {
 
-	public ArrayList<Offer> Offers;
+	public ArrayList<Offer> offfers;
 	
-	public OfferManager(ArrayList<Offer> Offers, int ask_price, int ask_area, int ask_employee_nbr){
-		Offers = OfferSorter(Offers, ask_price, ask_area, ask_employee_nbr);
-		this.Offers = Offers;
+	public OfferManager(ArrayList<Offer> offers, int ask_price, int ask_area, int ask_employee_nbr){
+		offers = OfferSorter(offers, ask_price, ask_area, ask_employee_nbr);
+		this.offfers = offers;
 	}
 	
-    public ArrayList<Offer> OfferSorter(ArrayList<Offer> Offers, int ask_price, int ask_area, int ask_employee_nbr) {
-        for (int i = 0; i < Offers.size(); i++){
-            Offer ofr_i = Offers.get(i);
+    public ArrayList<Offer> OfferSorter(ArrayList<Offer> offers, int ask_price, int ask_area, int ask_employee_nbr) {
+        for (int i = 0; i < offers.size(); i++){
+            Offer ofr_i = offers.get(i);
             double priice = (ofr_i.theprice + (ofr_i.theprice*(0.05)));
             if (priice > ask_price){
-                Offers.remove(i);
+            	System.out.println("if price");
+                offers.remove(i);
+                i-=1;
             }
             else {
                 double diff = (ask_price - (ofr_i.theprice))/(ofr_i.theprice);
                 double pricescore = diff*(-2);
                 ofr_i.thescore = ofr_i.thescore + pricescore;
+                System.out.println("else price");
             }
         }
-        for (int i = 0; i < Offers.size(); i++){
-            Offer ofr_i = Offers.get(i);
-            double area = ofr_i.thearea + (ofr_i.thearea*0.1);
-            if (area < ask_area){
-                Offers.remove(i);
+        System.out.println("offers.size() " + offers.size());
+        for (int i = 0; i < offers.size(); i++){
+            Offer ofr_i = offers.get(i);
+            double area = ofr_i.thearea*1.1;
+            
+            if (area <= ask_area){
+            	 System.out.println("if area");
+                offers.remove(i);
+                i-=1;
             }
+            
             else {
                 double diff = (area - ask_area)/ask_area;
                 double areascore = diff*(-1);
                 ofr_i.thescore = ofr_i.thescore + areascore;
+                System.out.println("else area ");
             } 
-        }
-        for (int i=0; i < Offers.size(); i++){
-            Offer ofr_i = Offers.get(i);
-            if (ofr_i.themax_employee_number > ask_employee_nbr){
-                Offers.remove(i);
+        }  
+        for (int i=0; i < offers.size(); i++){
+            Offer ofr_i = offers.get(i);
+            if (ofr_i.themax_employee_number < ask_employee_nbr){
+            	System.out.println("if employee");
+            	offers.remove(i);
+                i-=1;
+                
             }
             else {
                 int diff = (ask_employee_nbr - ofr_i.themax_employee_number)/ofr_i.themax_employee_number;
                 double employeescore = diff*(-1);
                 ofr_i.thescore = ofr_i.thescore + employeescore;
+                System.out.println("themax_employee_number " + ofr_i.themax_employee_number);
+                System.out.println("else employee");
             }
         }
-        Collections.sort(Offers, Offer.OfferComparator);
+        Collections.sort(offers, Offer.OfferComparator);
        
         
-    return Offers;
+    return offers;
     }
 }
-    
-    
-    
-    
-    
-    
-       /* HashMap<Offer, Integer> hm = new HashMap<Offer, Integer>();
-        for (int i; i < Offers.size(); i++){
-            Offer ofr_i = Offers.get(i);
-            hm.put(ofr_i, ofr_i.thescore);
-            //caster int en Integer ?
-        }
-
-        //Plus qu'a trier la HashMap par Valeur et avoir les meillers offres !*/
 
 
 
